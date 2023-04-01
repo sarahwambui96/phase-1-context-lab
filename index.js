9c793f621c5becfef0aed9e69a0b177e09cb7f91
sarahwambui96 <sarahwambui843@gmail.com>
2023-04-02 00:50:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
 /* Your Code Here */
+//function createEmployeeRecord
+function createEmployeeRecord(array){
+    return  {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+// function createEmployeeRecords
+function createEmployeeRecords(arrays){
+    return arrays.map(createEmployeeRecord)
+}
+
+//function createTimeInEvent
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(" ")[1], 10),
+        date: dateStamp.split(" ")[0]
+    })
+    return this;
+}
+
+//function createTimeOutEvent
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(" ")[1], 10),
+        date: dateStamp.split(" ")[0]
+    })
+    return this;
+}
+
+//calculate the hours worked
+function hoursWorkedOnDate(dateHours){
+    const timeInHour = this.timeInEvents.find(dateStamp => dateStamp.date === dateHours).hour
+    const timeOutHour = this.timeOutEvents.find(dateStamp => dateStamp.date === dateHours).hour
+    const totalHours = (timeOutHour - timeInHour)
+    return totalHours / 100
+}
+
+//calculate wages earned on date
+function wagesEarnedOnDate(dateHours){
+    const wagedHours = hoursWorkedOnDate.call(this, dateHours);
+    const employeeWage = wagedHours * this.payPerHour;
+    return parseFloat(employeeWage.toString());
+}
+
+//find employee by first name
+function findEmployeeByFirstName(emptyArray, firstName){
+    return emptyArray.find((employee) => {
+        return employee.firstName === firstName;
+    });
+}
+
+// calculate payroll
+function calculatePayroll(employeeArray){
+    return employeeArray.reduce((accumulator, record) =>{
+        return accumulator + allWagesFor.call(record);
+    }, 0);
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +84,9 @@ const allWagesFor = function () {
 
     return payable
 }
+
+//create employee record function
+
+
+
 
